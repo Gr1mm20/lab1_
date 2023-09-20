9257aa5b5a37c257fce933acaf3e031530bbf032
Gr1mm20 <145190344+Gr1mm20@users.noreply.github.com>
2023-09-20 11:50:26 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,25 +1,19 @@
 #Написать функцию, которая принимает список, состоящий из n элементов,
 # и целое число x и с помощью генераторной функции выводит на консоль только те элементы
 # входящего списка, которые больше числа x.
-
 def list_full(list_n):
-
     list1 = []
     for i in range(list_n):
         print("Введите целое число для ", i + 1, "элемента списка")
-        list1[i] = list1.append(input())
+        num = int(input())
+        list1.append(num)
     return list1
+def compare_yield (list1,x):
+    generator = (element for element in list1 if element > x)
+    for element in generator:
+        print(element)
 
-def compare_yield (list_n,x,list1):
-    for i in range(list_n):
-        if x > list1[i]:
-            continue
-        else:
-            yield
-
-
-x = int(input("Введите целое число х: "))
 n = int(input("Введите число элементов списка: "))
-list1=list_full(n)
-compare_generator = compare_yield(n,x,list1)
-print(compare_generator)
+list1 = list_full(n)
+x = int(input("Введите целое число х: "))
+compare_yield(list1,x)
