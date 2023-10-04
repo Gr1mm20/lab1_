35cce4eaff834a83d99573325fb189ec63b7d882
Gr1mm20 <145190344+Gr1mm20@users.noreply.github.com>
2023-10-04 17:45:05 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,1 +1,19 @@
+from datetime import datetime, timedelta
 
+def get_time_gmt(gmt):
+    try:
+        delta = timedelta(hours=int(gmt))
+        current_time = datetime.utcnow()
+        result_time = current_time + delta
+        return result_time
+    except ValueError:
+        return None
+
+
+gmt_ = input("Введите смещение по GMT:\t")
+result = get_time_gmt(gmt_)
+if result:
+    print("Смешанное значение даты и времени:", result)
+else:
+    print("Неверный формат GMT строки.")
+  
